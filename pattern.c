353cb79e5cc637e77f25d5228aaef6c2c092ade0
Habibur <habiburnabiarafat@gmail.com>
2024-05-04 19:25:04 +0600

    pattern 1 solved

--- pattern.c
+++ pattern.c
@@ -1,6 +1,41 @@
 #include<stdio.h>
-int main(){
-    
-
+int main()
+{
+    int input,space,pattern;
+    scanf("%d",&input);
+    space=input-1;
+    pattern=1;
+    for(int i=1;i<=(2*input)-1;i++)
+    {
+        // ekta line
+        for(int j=1;j<=space;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=pattern;j++)
+        {
+            if (i%2==0)
+            {
+               printf("-");
+            }
+            else{
+                printf("#");
+            }
+            
+            
+        }
+        // line seshe
+        if(i<=input-1)
+        {
+            space--;
+            pattern=pattern+2;
+        }
+        else
+        {
+            space++;
+            pattern=pattern-2;
+        }
+        printf("\n");
+    }
     return 0;
 }
